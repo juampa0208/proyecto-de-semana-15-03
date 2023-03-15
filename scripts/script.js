07ae6e842ac3819015af8411d75a703d947b1e4f
vlecc0808 <juanzapatapatino@hiesantaelena.edu.co>
2023-03-15 16:22:39 -0500

    modificar html y javascript

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,32 @@
+let nombre, correo, mensaje;
+let formulario = document .getElementById('form');
+
+formulario.addEventlistener('submit', (e)=>{
+    e.preventdefault() 
+    Leerdatos()
+})
+
+
+function Leerdatos() {
+    nombre = document.getElementById('nombre').value 
+correo = document.getElementById('correo').vale
+mensaje = document.querySelector('#textarea').vale 
+}
+
+function validarData(nombre,correo,mensaje) {
+    if(|nombre.legth==0 || correo.legth==0 || mensaje.legth==0) {
+
+        swal.fire({
+            title: 'Error',
+            text: 'do you want to continue',
+            icon: 'error'
+            confirmButtontext: 'cool',
+            iconcolor: 'purple'
+
+    
+        })
+    }
+}
+ 
+
 
